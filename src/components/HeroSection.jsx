3dfe9abf34a8d404eee4c5009e158cc3802dc9c2
farah <farahabouhassoun7@gmail.com>
2026-02-27 12:51:02 +0300

    Update Hero Section Styles

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -14,27 +14,26 @@ import person4 from "../assets/images/person4-hero.avif"
 const HeroSection = () => {
   return (
 
-   <section className='center-conatiner min-h-screen '>
-    <h1 className='text-[80px] font-bold text-text-black max-w-[472px]'>Take over the timeline</h1>
-    <p className='text-text-secondary text-[20px] font-medium '>Unlock the content tactics creators use to blow up fast and stay at the top of the feed.</p>
-    <div className='flex gap-3'>
+   <section className='center-conatiner min-h-screen flex justify-center items-center flex-col gap-3.5 relative'>
+    <ShapesOnTheSide className='absolute top-[25%] left-[10%] -rotate-12' img={eye} number={8600}/>
+    <ShapesOnTheSide className='absolute top-[35%] left-[5%] rotate-12' img={heart} number={1520}/>
+    <ShapesOnTheSide className='absolute top-[25%] right-[10%] rotate-12' img={message} number={1160}/>
+    <ShapesOnTheSide className='absolute top-[35%] right-[5%] -rotate-12' img={bookMarker} number={730}/>
+
+    <h1 className='text-[85px] font-bold text-text-black max-w-[500px] text-center leading-[90px]'>Take over the timeline</h1>
+    <p className='text-text-secondary text-[22px] font-medium text-center max-w-[480px]'>Unlock the content tactics creators use to blow up fast and stay at the top of the feed.</p>
+    
+    <div className='flex gap-3 mt-2 mb-12'>
       <EnrollButton text='Enroll Now'/>
       <EnrollButton text='See Details' className='bg-gray-200! text-black! '/>
-
     </div>
-    <ShapesOnTheSide img={eye} number={+8600}/>
-    <ShapesOnTheSide img={heart} number={+1520}/>
-    <ShapesOnTheSide img={message} number={+1160}/>
-    <ShapesOnTheSide img={bookMarker} number={+730}/>
 
-    <div className='flex gap-1'>
-      <div className=' shadow-[0 12px 20px 0 var(--black-opacity--10)] bg-white rounded-[32px]'><img className='rounded-[28px]'  src={person1} alt="person"/> </div>
-      <div className=''><img src={person2} alt="person"/> </div>
-      <div className=''><img src={person3} alt="person"/> </div>
-      <div className=''><img src={person4} alt="person"/> </div>
+    <div className='flex gap-[-40px] items-center justify-center'>
+      <div className='background-img-hero-section -rotate-[7deg] z-10'><img className='img-hero-section' src={person1} alt="person"/> </div>
+      <div className='background-img-hero-section rotate-[6deg] translate-y-[-20px] ml-[-40px] z-20'><img className='img-hero-section' src={person2} alt="person"/> </div>
+      <div className='background-img-hero-section -rotate-[7deg] ml-[-40px] z-30'><img className='img-hero-section' src={person3} alt="person"/> </div>
+      <div className='background-img-hero-section rotate-[6deg] translate-y-[-20px] ml-[-40px] z-40'><img className='img-hero-section' src={person4} alt="person"/> </div>
     </div>
-    
-    
     
    </section>
   )
--- src/components/ShapesOnTheSide.jsx
+++ src/components/ShapesOnTheSide.jsx
@@ -1,6 +1,6 @@
-const ShapesOnTheSide = ({img,number}) => {
+const ShapesOnTheSide = ({img,number,className=''}) => {
   return (
-    <div className="flex gap-1 py-1 px-3 bg-bg-primary shapes-on-the-side-shadow w-fit rounded-[20px] ">
+    <div className={`flex gap-1 py-1 px-3 bg-bg-primary shapes-on-the-side-shadow w-fit rounded-[20px] ${className}`}>
         <div className="w-4 h-4 mt-1"><img src={img} alt="" /></div>
         <div className="text-text-secondary font-medium ">{number}</div>
         </div>
